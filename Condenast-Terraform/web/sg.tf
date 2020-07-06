dc41abae45eed11c49573166bbff1ce060b9ac42
Jitendar Singh <jitsingh@redhat.com>
2020-07-06 10:38:27 +0530

    Update sg.tf

--- Condenast-Terraform/web/sg.tf
+++ Condenast-Terraform/web/sg.tf
@@ -1,11 +1,23 @@
 resource "aws_security_group" "web_sg"{
     name = "web_sg"
+    ingress {
+        description = "SSH"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     ingress{
         to_port = 8000
         from_port = 8000
         protocol = "TCP"
         cidr_blocks = ["0.0.0.0/0"]
-
     }
     egress{
         to_port = 8000
@@ -13,4 +25,5 @@ resource "aws_security_group" "web_sg"{
         protocol = "TCP"
         cidr_blocks = ["0.0.0.0/0"]
     }
+
 }
